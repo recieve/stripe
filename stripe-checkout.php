1a5ee233220e8ed6c58b905deec7db26f1e64aba
pderksen <pderksen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-09 17:34:15 +0000

    Uploading 1.4.1
    git-svn-id: https://plugins.svn.wordpress.org/stripe/trunk@1216452 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stripe-checkout.php
+++ stripe-checkout.php
@@ -1,26 +1,15 @@
 <?php
-
 /**
- * WP Simple Pay Lite for Stripe
- *
- * @package   SC
- * @author    Phil Derksen <pderksen@gmail.com>, Nick Young <mycorpweb@gmail.com>
- * @license   GPL-2.0+
- * @link      http://wpsimplepay.com
- * @copyright 2014-2015 Phil Derksen
- *
- * @wordpress-plugin
  * Plugin Name: WP Simple Pay Lite for Stripe
  * Plugin URI: http://wpsimplepay.com
  * Description: Add highly optimized Stripe checkout form overlays to your site in a few simple steps.
- * Version: 1.4.0.2
- * Author: Phil Derksen
- * Author URI: http://philderksen.com
- * License: GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * GitHub Plugin URI: https://github.com/pderksen/WP-Stripe-Checkout
+ * Author: Moonstone Media
+ * Author URI: http://moonstonemediagroup.com
+ * Version: 1.4.1
  * Text Domain: sc
  * Domain Path: /languages/
+ *
+ * Copyright 2014 Moonstone Media/Phil Derksen. All rights reserved.
  */
 
 // Exit if accessed directly.
@@ -28,48 +17,69 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Setup constant for main file path
-if ( ! defined( 'SC_MAIN_FILE' ) ) {
-	define( 'SC_MAIN_FILE', __FILE__ );
+// Plugin requirements.
+$stripe_checkout_requires = array(
+	'wp'  => '3.9.0',
+	'php' => '5.3.0',
+	'ext' => array( 'curl', 'mbstring' )
+);
+// Define constants.
+$stripe_checkout_constants = array(
+	'SC_REQUIRES'         => serialize( $stripe_checkout_requires ),
+	'SC_MAIN_FILE'        => __FILE__,
+	'SC_DIR_PATH'         => plugin_dir_path( __FILE__ ),
+	'SC_DIR_URL'          => plugin_dir_url( __FILE__ ) ,
+	'SC_WEBSITE_BASE_URL' => 'http://wpstripe.net/',
+);
+foreach( $stripe_checkout_constants as $constant => $value ) {
+	if ( ! defined( $constant ) ) {
+		define( $constant, $value );
+	}
 }
 
-// Set up constant for directory path
-if ( ! defined( 'SC_DIR_PATH' ) ) {
-	define( 'SC_DIR_PATH', plugin_dir_path( __FILE__ ) );
+// Check plugin requirements.
+include_once 'stripe-checkout-requirements.php';
+$stripe_checkout_requirements = new Stripe_Checkout_Requirements( $stripe_checkout_requires );
+if ( $stripe_checkout_requirements->pass() === false ) {
+
+	$stripe_checkout_fails = $stripe_checkout_requirements->failures();
+	if ( isset( $stripe_checkout_fails['wp'] ) || isset( $stripe_checkout_fails['php']) ) {
+
+		// Display an admin notice if running old WordPress or PHP.
+		function stripe_checkout_plugin_requirements() {
+			$required = unserialize( SC_REQUIRES );
+			global $wp_version;
+			echo '<div class="error"><p>' . sprintf( __( 'WP Simple Pay requires PHP %1$s and WordPress %2$s to function properly. PHP version found: %3$s. WordPress installed version: %4$s. Please update to meet the minimum requirements.', 'sc' ), $required['php'], $required['wp'], PHP_VERSION, $wp_version ) . '</p></div>';
+		}
+		add_action( 'admin_notices', 'stripe_checkout_plugin_requirements' );
+
+	}
+
+	if ( isset( $stripe_checkout_fails['ext'] ) ) {
+
+		// Display a notice if extensions are not found.
+		function stripe_checkout_plugin_extensions() {
+			$required = unserialize( SC_REQUIRES );
+			$extensions = '<code>' . implode( ', ', $required['ext'] ) . '</code>';
+			echo '<div class="error"><p>' . sprintf( __( 'WP Simple Pay requires the following PHP extensions to work: %s. Please make sure they are installed or contact your host.', 'sc' ), $extensions ) . '</p></div>';
+		}
+		add_action( 'admin_notices', 'stripe_checkout_plugin_extensions' );
+
+	}
+
+	// Halt the rest of the plugin execution if PHP check fails or extensions not found.
+	if ( isset( $stripe_checkout_fails['php'] ) || isset( $stripe_checkout_fails['ext'] ) ) {
+		return;
+	}
+
 }
 
-// Set up constant for Directory URL
-if ( ! defined( 'SC_DIR_URL' ) ) {
-	define( 'SC_DIR_URL', plugin_dir_url( __FILE__ ) );
-}
-
-// Website for this plugin
-if ( ! defined( 'SC_WEBSITE_BASE_URL' ) ) {
-	define( 'SC_WEBSITE_BASE_URL', 'http://wpstripe.net/' );
-}
-
-// Include main class file
-require_once( SC_DIR_PATH . 'classes/class-stripe-checkout.php' );
+// Load the plugin.
+require_once SC_DIR_PATH . 'classes/class-stripe-checkout.php';
 
 // Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
 register_activation_hook( SC_MAIN_FILE, array( 'Stripe_Checkout', 'activate' ) );
 
-// Check for minimum version of PHP before loading plugin.
-// Show admin notice if it doesn't.
-// https://github.com/WPupdatePHP/wp-update-php
-// plugin-name branch for now (to use set_plugin_name method)
-require_once( SC_DIR_PATH . 'libraries/WPUpdatePhp.php' );
-
-$updatePhp = new WPUpdatePhp( '5.3', '5.4' );
-$updatePhp->set_plugin_name( 'WP Simple Pay Lite for Stripe' );
-
-if ( $updatePhp->does_it_meet_required_php_version( PHP_VERSION ) ) {
-	
-	// TODO Uncomment recommended admin notice once it can be hidden by user.
-	// Show admin notice for recommended version of PHP, but if required version still met continue loading plugin.
-	//$updatePhp->does_it_meet_recommended_php_version( PHP_VERSION );
-	
-	// Set up global holding the base class instance so we can easily use it throughout
-	global $base_class;
-	$base_class = Stripe_Checkout::get_instance();
-}
+// Set up global holding the base class instance so we can easily use it throughout
+global $base_class;
+$base_class = Stripe_Checkout::get_instance();
